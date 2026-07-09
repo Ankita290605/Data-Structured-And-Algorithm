842c097870cd5a0f0e02637e3103851210a2d3ec
Ankita290605 <151769468+Ankita290605@users.noreply.github.com>
2026-07-10 01:49:46 +0530

    Time: 1 ms (81.41%), Space: 42.8 MB (69.7%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        for(char ch='a';ch<='z';ch++){
+            if(!sentence.contains(""+ch)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
