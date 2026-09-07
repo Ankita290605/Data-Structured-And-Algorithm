de5e01adea631731cae484fdb7cc9e3689027fcf
Ankita290605 <151769468+Ankita290605@users.noreply.github.com>
2026-09-07 18:53:49 +0530

    Time: 9 ms (12.78%), Space: 45.3 MB (94.19%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int[] arr = new int[4];
+
+        for(int i=0;i<moves.length();i++){
+            if(moves.charAt(i)=='R'){
+                arr[0]++;
+            }
+            else if(moves.charAt(i)=='L'){
+                arr[1]++;
+            }
+            else if(moves.charAt(i)=='U'){
+                arr[2]++;
+            }
+            else{
+                arr[3]++;
+            }
+        }
+
+        if(arr[0]!=arr[1] || arr[2]!=arr[3]){
+            return false;
+        }
+
+        return true;
+    }
+}
