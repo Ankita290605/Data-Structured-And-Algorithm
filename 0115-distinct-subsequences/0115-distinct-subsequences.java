af17ff0eeb8e131eac2b9b4bba32aa281316d976
Ankita290605 <151769468+Ankita290605@users.noreply.github.com>
2026-09-08 00:20:08 +0530

    Time: 20 ms (53.39%), Space: 53.9 MB (79.54%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,28 @@
+class Solution {
+    int[][] memo;
+    public int numDistinct(String s, String t) {
+        memo = new int[s.length()][t.length()];
+        for(int[] arr : memo){
+            Arrays.fill(arr, -1);
+        }
+        return solve(s, t, 0, 0);
+    }
+
+    public int solve(String s, String t, int i, int j) {
+
+        if(j == t.length()){
+            return 1;
+        }
+
+        if(i == s.length()){
+            return 0;
+        }
+
+        if(memo[i][j]!=-1) return memo[i][j];
+        if(s.charAt(i) == t.charAt(j)){
+            return memo[i][j] = solve(s, t, i+1, j+1) + solve(s, t, i+1, j);
+        }
+
+        return memo[i][j] = solve(s, t, i+1, j);
+    }
+}
