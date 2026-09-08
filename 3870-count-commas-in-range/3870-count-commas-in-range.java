7102f3204eb5226412d418f71cbf57e4df019fd1
Ankita290605 <151769468+Ankita290605@users.noreply.github.com>
2026-09-08 16:02:05 +0530

    Time: 233 ms (5.4%), Space: 46.6 MB (8.32%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -2,6 +2,9 @@ class Solution {
     public int countCommas(int n) {
         int count = 0;
 
+        String t = Integer.toString(n);
+        if(t.length()<4) return 0;
+
         for (int i = 1000; i <= n; i++) {
             String s = Integer.toString(i);
 
